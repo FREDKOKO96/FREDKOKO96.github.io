d8715c65bdf1d3fa11672f8b781df85ccbb9e178
FREDKOKO96 <hu284305@uaeh.edu.mx>
2023-08-11 16:36:57 -0600

    Update click-header.js

--- JS/click-header.js
+++ JS/click-header.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const subMenus = document.querySelectorAll(".submenu");
+    const menuLinks = document.querySelectorAll("nav ul li a");
   
     subMenus.forEach((submenu) => {
-      submenu.addEventListener("click", function () {
-        if (!submenu.classList.contains("active")) {
-          closeAllSubMenus();
-        }
-        submenu.classList.toggle("active");
+      submenu.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita la navegación si hay un enlace real
+        closeAllSubMenus();
+        this.classList.toggle("active");
       });
     });
   
@@ -18,16 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
   
     const checkBtn = document.querySelector(".checkbtn");
     const navUl = document.querySelector("nav ul");
-    const menuLinks = document.querySelectorAll("nav ul li a");
   
     checkBtn.addEventListener("click", function () {
       navUl.classList.toggle("active");
-      closeAllSubMenus(); // Close all submenus when mobile menu is toggled
+      closeAllSubMenus(); // Cierra todos los submenús al mostrar el menú móvil
     });
   
     menuLinks.forEach((link) => {
       link.addEventListener("click", function () {
         navUl.classList.remove("active");
+        closeAllSubMenus(); // Cierra todos los submenús al hacer clic en un enlace principal
       });
     });
   });
